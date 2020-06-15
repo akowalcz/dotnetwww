1700caa2e53af5d0add173cac28e9315d90d290c
Andrzej Kowalczyk <akowalcz@akowalcz.remote.csb>
2020-06-15 12:48:09 +0200

    generated docs

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace webapp
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World From .Net container on Linux !");
             });
         }
     }
