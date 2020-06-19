6ae55fcb46b4f4438ef6572d1c43a7c252ce4570
akowalcz <akowalcz@redhat.com>
2020-06-19 12:34:18 +0200

    Update Startup.cs
    two params

--- Startup.cs
+++ Startup.cs
@@ -27,8 +27,10 @@ namespace webapp
 
             app.Run(async (context) =>
             {
-                string a = Environment.GetEnvironmentVariable("DBNameParam");
-                await context.Response.WriteAsync("Hello World From .Net Core container on Linux Jun 19 at 11:32! " + a);
+                string dbName = Environment.GetEnvironmentVariable("DBNameParam");
+                string dbUser = Environment.GetEnvironmentVariable("DBuserParam");               
+                await context.Response.WriteAsync("Hello World From .Net Core container on Linux Jun 19 at 11:32! " + dbName);
+                await context.Response.WriteAsync("Database name: " + dbName "User name: "  + dbUser);
             });
         }
     }
