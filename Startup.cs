d1cc9ad131222d15c93af1958fa003f18c530609
akowalcz <akowalcz@redhat.com>
2020-09-16 08:53:43 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace webapp
                 string dbUser = Environment.GetEnvironmentVariable("DBUser");
                 string dbPass = Environment.GetEnvironmentVariable("dbpasswd");
                                 
-                await context.Response.WriteAsync("Hello World From .Net Core container Sep 2 at 11:11! running on: ");
+                await context.Response.WriteAsync("Hello World From .Net Core container Sep 16 at 11:11! running on: ");
                 
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
