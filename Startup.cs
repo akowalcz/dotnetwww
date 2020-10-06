20a987e0690c50936c2d1eb3b0c5ca841e0361f1
akowalcz@redhat.com <akowalcz@redhat.com>
2020-10-06 15:53:54 +0000

    new time

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace webapp
                 string dbUser = Environment.GetEnvironmentVariable("DBUser");
                 string dbPass = Environment.GetEnvironmentVariable("dbpasswd");
                                 
-                await context.Response.WriteAsync("Hello World From .Net Core container Oct 6 at 17:46! running on: ");
+                await context.Response.WriteAsync("Hello World From .Net Core container Oct 6 at 17:53! running on: ");
                 
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
                 {
